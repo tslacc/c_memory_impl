e23c7fdb85b9ca6dab458591d18457fced9c5c89
tslacc <46695088+tslacc@users.noreply.github.com>
2026-07-22 16:01:28 -0400

    Store memory size
    Store the size of each block in a 4-byte wide space before the memory block

--- malloc.c
+++ malloc.c
@@ -7,15 +7,22 @@ typedef struct block{
 	char in_use;
 } s_block;
 
-s_block *mem_blocks;
 
+s_block *mem_blocks;
+size_t *base_address;
 //On calling malloc, determine the size of the block necessary.
 //Refer to https://linux.die.net/man/3/malloc
 
 void *_malloc(size_t size){
-	printf("malloc called with size %u\n",size);
-	printf("sbrk(0) returns with value %u\n", sbrk(0));
+	if(base_address == NULL){
+		base_address = sbrk(0);
+	}
+	printf("Base address starts at %u\n", base_address);
 	//If size is smaller than mmap size, sbrk. If larger, then mmap strategy
 	//Mutex possibly necessary
+	//Write the size of the block before the block itself. Limit 4-byte block size
+	if(!(size < 4294967296)) return NULL;
+	*(unsigned int*)sbrk(4) = size;
+	printf("Allocating %u size at position %u, size written to %u\n", size, sbrk(0), sbrk(0)-4);
 	return sbrk(size);
 }
