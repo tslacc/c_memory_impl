0c445a5ece9c5830b974d6df1f7f1ef98b0b355d
tslacc <46695088+tslacc@users.noreply.github.com>
2026-07-23 15:15:52 -0400

    Thread safety mutex
    Basic naive spinlock with static char

--- malloc.c
+++ malloc.c
@@ -1,17 +1,18 @@
 #include "malloc.h"
 
 
-
+/*On calling malloc, determine the size of the block necessary.
+//Refer to https://linux.die.net/man/3/malloc
 typedef struct block{
 	char *data;
 	char in_use;
 } s_block;
-
-
 s_block *mem_blocks;
+*/
+
 size_t *base_address;
-//On calling malloc, determine the size of the block necessary.
-//Refer to https://linux.die.net/man/3/malloc
+
+static char mutex = 0;
 
 void *_malloc(size_t size){
 	if(base_address == NULL){
@@ -22,7 +23,10 @@ void *_malloc(size_t size){
 	//Mutex possibly necessary
 	//Write the size of the block before the block itself. Limit 4-byte block size
 	if(!(size < 4294967296)) return NULL;
+	while(mutex!=0); //Basic spinlock
+	mutex=1;
 	*(unsigned int*)sbrk(4) = size;
 	printf("Allocating %u size at position %u, size written to %u\n", size, sbrk(0), sbrk(0)-4);
+	mutex=0;
 	return sbrk(size);
 }
